7dd4d530580754fa21ad942ab8a5e6fe5f56823d
Linards Smeiksts <linardssmeiksts@gmail.com>
2021-02-23 00:08:47 +0200

    Starting a new project - multiroom chat

---
+++ chatroom-many-rooms/server.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+void printMenu(void);
+
+
+int main()
+{
+	
+	printMenu();
+
+	return 0;
+}
+
+void printMenu(void)
+{
+	cout << "============= WELCOME TO CHAT ROOM ===============\n";
+	cout << "=============== CHOOSE AN OPTION =================\n";
+	cout << "JOIN ROOM (1)\n";
+	cout << "CREATE ROOM (2)\n";	
+	cout << "CHANGE OPTIONS (3)\n";	
+	cout << "QUIT (4)\n";
+	cout << "==================================================\n";
+	cout << "==================================================\n";
+
+}
